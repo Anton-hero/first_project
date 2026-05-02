d8a3ec600fba62a89dddaa022dd3b7def9ad1e87
Anton Egorov <EropoB.A.IO@yandex.ru>
2026-05-02 13:37:31 +1000

    Add dotenv and print_author function with .env in .gitignore

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Hello from repository!")
+# Импорт библиотек
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    # Загружаем переменные из файла .env
+    load_dotenv()
+    
+    # Читаем переменную AUTHOR
+    author = os.getenv('AUTHOR')
+    
+    # Печатаем имя автора
+    print(f"Автор проекта: {author}")
+
+# Вызов функции для проверки
+if __name__ == "__main__":
+    print_author()
